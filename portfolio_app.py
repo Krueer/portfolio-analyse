4563166edcc859cb9894534213a1f7cd5eb691bc
Krueer <85999884+Krueer@users.noreply.github.com>
2026-08-06 12:32:01 +0200

    Fix syntax error in portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -1397,7 +1397,7 @@ open_df.loc[open_df["ISIN"] == "Physisches Cash", "invested"] = total_cash
 open_df.loc[open_df["ISIN"] == "Physisches Cash", "avg_cost"] = 1.0
 
 open_df.loc[open_df["ISIN"] == "Andere Assets", "invested"] = total_other_assets
-open_df.loc[open_df["Andere Assets", "avg_cost"] = 1.0
+open_df.loc[open_df["ISIN"] == "Andere Assets", "avg_cost"] = 1.0
 
 open_df.loc[open_df["ISIN"] == "Offene Kredite", "invested"] = -total_loans
 open_df.loc[open_df["ISIN"] == "Offene Kredite", "avg_cost"] = 1.0
